a4b2e643694a3179e0ec9029c4d5b5b4ee18fb4e
vvalotto <vvalotto@gmail.com>
2019-04-30 14:06:34 -0300

    Inicio: Lanzar App Flask

---
+++ run.py
@@ -0,0 +1,13 @@
+'''
+Lanza aplicacion Falsk
+'''
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=4000)
